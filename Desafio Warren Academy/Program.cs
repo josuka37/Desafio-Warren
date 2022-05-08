883e93921a6633992f64c9b34a35905f45001352
Josué da Motta <josuka360@hotmail.com>
2022-05-08 15:23:56 -0300

    Documentação corrigida

--- Desafio Warren Academy/Program.cs
+++ Desafio Warren Academy/Program.cs
@@ -8,20 +8,26 @@ namespace Desafio_Warren_Academy
         {
             int invertido = 0, numero = 0, soma = 0;
 
-
+             /// Verifica todos os números a partir de 10 até 1 milhão
              for (int i = 10; i < 1000000; i++)
              {
+                ///Desconsidera os números que terminam com zero.
                 if (i%10 != 0)
                 {
+                    ///Faz a inversão do número
                      numero = i;
                      while (numero > 0)
                      {
                          invertido = invertido * 10 + numero % 10;
                          numero /= 10;
                      }
+                        ///faz a soma do número com o invertido 
                         soma = i + invertido;
+
+                        ///Verifica se a soma do número com o invertido resulta em número ímpar abaixo de 1 milhão, se sim, imprime resultado.
                      if ((soma)%2 != 0 && soma <1000000)
                      {
+                        ///imprime resultado
                         Console.WriteLine(i +" + " + invertido +" = "+ soma);
                      }
                         invertido = 0;
