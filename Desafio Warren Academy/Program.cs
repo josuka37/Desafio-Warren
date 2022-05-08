ed7c4c7790a0502762b9dc207170c93c9a5a813b
Josué da Motta <josuka360@hotmail.com>
2022-05-08 13:34:38 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio Warren Academy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Desafio_Warren_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int invertido = 0, numero = 0, soma = 0;
+
+
+             for (int i = 10; i < 1000000; i++)
+             {
+                if (i%10 != 0)
+                {
+                     numero = i;
+                     while (numero > 0)
+                     {
+                         invertido = invertido * 10 + numero % 10;
+                         numero /= 10;
+                     }
+                        soma = i + invertido;
+                     if ((soma)%2 != 0 && soma <1000000)
+                     {
+                        Console.WriteLine(i +" + " + invertido +" = "+ soma);
+                     }
+                        invertido = 0;
+
+                }
+             }
+            
+            
+        }
+    }
+    
+}
