46fffcb58ef124b6abaf5a7f8bbcaff90853cbb1
Josué da Motta <86384044+josuka37@users.noreply.github.com>
2022-05-08 21:00:16 -0300

    Add files via upload

---
+++ Desafio Warren 2/Desafio Warren 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Desafio_Warren_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3, contador = 0; int[] tempoChegada = new int[5] { -2, -1, 0, 1, 2 };
+
+            for (int i = 0; i < tempoChegada.Length; i++)
+            {   
+                ///conta quantos alunos chegaram atrasado, com tempo zero ou negativo.
+                if (tempoChegada[i] <= 0)
+                {
+                    contador++;
+                }
+            }
+            //verifica se x alunos chegaram a tempo.
+            if (contador >= x)
+            {
+                Console.WriteLine("Aula Normal");
+            }
+            /// caso não, aula cancelada.
+            else
+            {
+                Console.WriteLine("Aula Cancelada ");
+            }
+        }
+    }
+}
